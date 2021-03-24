2818d6963e39626e1323956bdfcc5a5335ce36eb
linwei <linweilifudan@gmail.com>
2021-03-24 11:28:10 +0800

    disable the warning of using deprecated NumPy API

--- topic_models/sample/setup.py
+++ topic_models/sample/setup.py
@@ -7,6 +7,7 @@ ext = Extension(
         name='_sample',
         sources=['_sample.pyx'],
         include_dirs=[np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
 )
 
 setup(
